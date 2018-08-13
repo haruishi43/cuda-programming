fdcdaf6c637a15ee09b98817a17216c52af96abe
Toraudonn <toraudonn@gmail.com>
2018-08-13 18:13:49 +0900

    pybind11 cuda demo

---
+++ pybind-cuda-demo/build/CMakeFiles/cuda_pybind.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_pybind.dir/main.cpp.o"
+  "cuda_pybind.pdb"
+  "cuda_pybind.cpython-36m-x86_64-linux-gnu.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cuda_pybind.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
